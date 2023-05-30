722b16dbec9e9e6be424934be3c725c150c04f5e
GeralmyUrena <geralmyuri11@gmail.com>
2023-05-30 18:45:45 -0400

    commit tree 1a72656cadcebbcbcb9493dc2b2254e91d85c295

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
+class Book
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+    attr_accessor :turn_page
+    def initialize (title)
+        @title = title
+        
+    end
+    def turn_page
+        @page_count = puts "Flipping the page...wow, you read fast!"
+    end
+    
+end
 
 
+# book = Book.new()
+
+# book.turn_page
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
 # Make your shoe class here!
+class Shoe 
+  attr_reader :brand
+  attr_accessor :color
+  attr_accessor :size
+  attr_accessor :material
+  attr_accessor :condition
+  attr_writer :cobble
+   def initialize (brand="Nike")
+        @brand = brand
+   end
+   def cobble
+    puts "Your shoe is as good as new!"
+    @condition =   "new"
+   
+   end
+end
+
+# shoe = Shoe.new
+# shoe.cobble= "Your shoe is as good as new!"
+# shoe.condition="old"
